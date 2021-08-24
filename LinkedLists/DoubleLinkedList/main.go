fef63e85a0fc0b82b4dcd0bd0e6574c6b35548ab
Tan Le Minh <v.tanlm2@vinfast.vn>
2021-08-24 21:57:49 +0700

    find, remove

--- LinkedLists/DoubleLinkedList/main.go
+++ LinkedLists/DoubleLinkedList/main.go
@@ -44,6 +44,59 @@ func (l *DoublyLinkedList) AddTail(data int) {
 	}
 }
 
+func (l *DoublyLinkedList) Find(data int) *DoublyLinkedListNode {
+
+	current := l.Head
+
+	for current != nil {
+		if current.Data == data {
+			return current
+		}
+		current = current.Next
+	}
+	return nil
+}
+
+func (l *DoublyLinkedList) Remove(data int) bool {
+	found := l.Find(data)
+	if found == nil {
+		return false
+	}
+
+	next := found.Next
+	prev := found.Prev
+
+	/* remove HEAD */
+	if prev == nil {
+		l.Head = next
+		if l.Head != nil {
+			l.Head.Prev = nil
+		}
+	} else {
+		prev.Next = next
+
+	}
+
+	/* remove TAIL */
+	if next == nil {
+		l.Tail = prev
+		if l.Tail != nil {
+			l.Tail.Next = nil
+		}
+	} else {
+		next.Prev = prev
+	}
+
+	return true
+}
+
+func (l *DoublyLinkedList) printf() {
+	fmt.Println("Printing...")
+	for n := l.First(); n != nil; n = n.Nexte() {
+		fmt.Printf("%v\n", n.Data)
+	}
+}
+
 func main() {
 	l := new(DoublyLinkedList)
 
@@ -51,7 +104,36 @@ func main() {
 	l.AddHead(12)
 	l.AddTail(1991)
 
-	for n := l.First(); n != nil; n = n.Nexte() {
-		fmt.Printf("%v\n", n.Data)
+	l.printf()
+
+	node := l.Find(1992)
+	if node != nil {
+		fmt.Printf("%v is exist\n", node.Data)
+	} else {
+		fmt.Printf("is not exist\n")
+	}
+
+	if ok := l.Remove(17); ok {
+		l.printf()
+		l.AddHead(17)
+		l.printf()
+	}
+
+	head := l.Head.Data
+	if ok := l.Remove(head); ok {
+		l.printf()
+		l.AddHead(head)
+		l.printf()
+	}
+
+	tail := l.Tail.Data
+	if ok := l.Remove(tail); ok {
+		l.printf()
+		l.AddTail(tail)
+		l.printf()
+	}
+
+	if ok := l.Remove(1992); ok {
+		l.printf()
 	}
 }
